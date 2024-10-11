095ad943a1a01706a9c7f42a7dd373aadcdc3770
Ibrahim Khalil <ibrahimkholil01@gmail.com>
2024-10-11 16:57:24 +0600

    Update production

---
+++ MVC Project/Models/Search.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project.Models
+{
+    public class Search
+    {
+        public string BranchName { get; set; }
+        public int BranchCode { get; set; }
+
+        public static List<Search> GetSearch()
+        {
+            List<Search> lst = new List<Search>();
+
+            Search search = new Search();
+            search.BranchName = "Bangladesh";
+            search.BranchCode = 110;
+            lst.Add(search);
+
+            search = new Search();
+            search.BranchName = "Arab";
+            search.BranchCode = 110;
+            lst.Add(search);
+
+
+            return lst;
+        }
+    }
+}
