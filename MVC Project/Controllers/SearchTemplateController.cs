a901171d0630f4b06a0ccaf11b947853b93b79d2
Ibrahim Khalil <ibrahimkholil01@gmail.com>
2024-10-11 11:37:30 +0600

    Huge Update

--- MVC Project/Controllers/SearchTemplateController.cs
+++ MVC Project/Controllers/SearchTemplateController.cs
@@ -13,5 +13,17 @@ namespace MVC_Project.Controllers
         {
             return View();
         }
+        public ActionResult Teacher()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public ActionResult Employee()
+        {
+            return View();
+        }
     }
 }
